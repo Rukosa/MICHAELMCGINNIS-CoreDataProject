1680a5424a8e4a3b54da5cfdc34b313c6eed4365
Rukosa <mwmcginnis490@gmail.com>
2022-03-30 14:15:16 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-CoreDataProject/MICHAELMCGINNIS_CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_CoreDataProjectApp.swift
+//  MICHAELMCGINNIS-CoreDataProject
+//
+//  Created by Michael Mcginnis on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
